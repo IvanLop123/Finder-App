f072566bedb557be3ce6fe08ec70bf8209810e66
IvanLop123 <146348083+IvanLop123@users.noreply.github.com>
2024-10-15 01:01:14 -0400

    second commit
    got app to launch live server

---
+++ netflix/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVnedEWTlw7cWvnqZQOoNso51OPCxJtn0",
+    authDomain: "finder-7deef.firebaseapp.com",
+    projectId: "finder-7deef",
+    storageBucket: "finder-7deef.appspot.com",
+    messagingSenderId: "494776973010",
+    appId: "1:494776973010:web:b9f784920da82de9c5df0b"
+  };
